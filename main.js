a0dff94fa443d29757a2a6bf3d242b4b66c85375
Angelika Borko <angieborko@gmail.com>
2023-08-20 10:34:02 +0200

    First js file

---
+++ main.js
@@ -0,0 +1,14 @@
+alert("Witaj! :)");
+console.log("jestem Twoim console.logiem");
+console.log("jestem Twoim drugim console.logiem");
+
+const firstName = "Angelika";
+const age = 34;
+
+console.log(firstName);
+console.log(age);
+
+//` - backtick / backquote
+// ${zmienna}
+
+console.log(`Nazywam się ${firstName} i mam ${age} lata`);
