fa1da08dd90951237334ab786b473fc171a4dd1f
Angelika Borko <angieborko@gmail.com>
2023-10-22 15:11:07 +0200

    Added notes from JS from module 5

--- main.js
+++ main.js
@@ -12,3 +12,25 @@ console.log(age);
 // ${zmienna}
 
 console.log(`Nazywam się ${firstName} i mam ${age} lata`);
+
+// funkcja
+
+function calculate(myNumber) {
+return myNumber*1;
+}
+
+console.log("My number from normal function is",calculate(7));
+
+// funkcja strzałkowa
+
+const calculateWithArrow = myNumber => {
+    return myNumber*2;
+    }
+    
+    console.log("My number from arrow function is",calculateWithArrow(7));
+    
+// funcja strzałkowa skrócona
+
+const calculateWithArrowShort = myNumber => (myNumber*1)*3;
+    
+    console.log("My number from shortened arrow function is",calculateWithArrowShort(7));
