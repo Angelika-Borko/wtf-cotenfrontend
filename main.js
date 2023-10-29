f9024c289b1f534d178142c9cb0510bdddf7777a
Angelika Borko <angieborko@gmail.com>
2023-10-29 11:36:37 +0100

    Objects and methods

--- main.js
+++ main.js
@@ -1,36 +1,58 @@
-// alert("Witaj! :)");
+    // alert("Witaj! :)");
+
 console.log("jestem Twoim console.logiem");
 console.log("jestem Twoim drugim console.logiem");
+
+    // funkcja powitalna z parametrami
+    //` - backtick / backquote
+    // ${zmienna}
 
 const firstName = "Angelika";
 const age = 34;
 
-console.log(firstName);
-console.log(age);
+console.log(`Nazywam się ${firstName} i mam ${age} lata`); 
 
-//` - backtick / backquote
-// ${zmienna}
-
-console.log(`Nazywam się ${firstName} i mam ${age} lata`);
-
-// funkcja
+    // funkcja
 
 function calculate(myNumber) {
-return myNumber*1;
+    return myNumber * 1;
 }
 
-console.log("My number from normal function is",calculate(7));
+console.log("My number from normal function is", calculate(7));
 
-// funkcja strzałkowa
+    // funkcja strzałkowa
 
 const calculateWithArrow = myNumber => {
-    return myNumber*2;
-    }
-    
-    console.log("My number from arrow function is",calculateWithArrow(7));
-    
-// funcja strzałkowa skrócona
+    return myNumber * 2;
+}
 
-const calculateWithArrowShort = myNumber => (myNumber*1)*3;
-    
-    console.log("My number from shortened arrow function is",calculateWithArrowShort(7));
+console.log("My number from arrow function is", calculateWithArrow(7));
+
+    // funcja strzałkowa skrócona
+
+const calculateWithArrowShort = myNumber => (myNumber * 1) * 3;
+
+console.log("My number from shortened arrow function is", calculateWithArrowShort(7));
+
+    // obiekty
+
+const deathStar = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} destroyed 💥`)
+    },
+    isOperating: true,
+    levels: 357,
+    name: 'Death Star',
+    population: 10000,
+    commander: { // zagnieżdżenie obiektu w obiekcie
+        name: 'darth vader' // druga własność name jest częścią kolejnego obiektu, więc może nazywać się w ten sam sposób
+    }
+}
+
+console.log(deathStar);
+console.log(deathStar.name);
+console.log(deathStar['diameter']);
+console.log(deathStar.commander.name);
+
+deathStar.fire('Russia');
